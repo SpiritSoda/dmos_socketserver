f6959f48c02a1a02ed027055196d2b7f6ea340f2
SpiritSoda <1800013119@pku.edu.cn>
2022-11-20 16:37:29 +0800

    [Adjust] adjust for change of dmos_common

--- src/main/java/com/dmos/dmos_socketserver/dmos_socket/component/DMOSSocketClientSide.java
+++ src/main/java/com/dmos/dmos_socketserver/dmos_socket/component/DMOSSocketClientSide.java
@@ -2,6 +2,7 @@ package com.dmos.dmos_socketserver.dmos_socket.component;
 
 import com.dmos.dmos_client.DMOSClientContext;
 import com.dmos.dmos_common.config.DMOSConfig;
+import com.dmos.dmos_common.data.DMOSRequest;
 import com.dmos.dmos_common.data.DMOSResponse;
 import com.dmos.dmos_common.util.HttpUtil;
 import com.dmos.dmos_common.util.Port;
@@ -11,6 +12,7 @@ import com.dmos.dmos_socketserver.dmos_socket.handler.DMOSSocketClientHandler;
 import com.google.gson.Gson;
 import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
 
 import javax.annotation.PostConstruct;
 import java.net.InetSocketAddress;
@@ -20,13 +22,14 @@ public class DMOSSocketClientSide {
     private final HttpUtil httpUtil = SpringUtil.getBean(HttpUtil.class);
     private final DMOSConfig dmosConfig = SpringUtil.getBean(DMOSConfig.class);
     private final DMOSClientContext clientContext = SpringUtil.getBean(DMOSClientContext.class);
+    private final RestTemplate restTemplate = SpringUtil.getBean(RestTemplate.class);
     @PostConstruct
     public void start(){
         String token = dmosConfig.getLocalToken();
         String url = dmosConfig.getStorage();
         HttpHeaders headers = new HttpHeaders();
         headers.add("token", token);
-        DMOSResponse response = httpUtil.post(url, "/token", headers, "{}");
+        DMOSResponse response = httpUtil.post(url, "/api/token", headers, new DMOSRequest(), restTemplate);
         int id = Integer.parseInt(response.getData().get("id"));
         clientContext.id(id);
         clientContext.token(response.getData().get("token"));
--- src/main/java/com/dmos/dmos_socketserver/dmos_socket/util/SocketServerHttpUtil.java
+++ src/main/java/com/dmos/dmos_socketserver/dmos_socket/util/SocketServerHttpUtil.java
@@ -1,6 +1,7 @@
 package com.dmos.dmos_socketserver.dmos_socket.util;
 
 import com.dmos.dmos_common.config.DMOSConfig;
+import com.dmos.dmos_common.data.DMOSRequest;
 import com.dmos.dmos_common.data.DMOSResponse;
 import com.dmos.dmos_common.data.ClientReportDTO;
 import com.dmos.dmos_common.util.HttpUtil;
@@ -8,6 +9,7 @@ import com.google.gson.Gson;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.RestTemplate;
 
 import java.util.HashMap;
 
@@ -15,11 +17,13 @@ import java.util.HashMap;
 public class SocketServerHttpUtil {
     private final DMOSConfig dmosConfig;
     private final HttpUtil httpUtil;
+    private final RestTemplate restTemplate;
 
     @Autowired
-    public SocketServerHttpUtil(DMOSConfig dmosConfig, HttpUtil httpUtil) {
+    public SocketServerHttpUtil(DMOSConfig dmosConfig, HttpUtil httpUtil, RestTemplate restTemplate) {
         this.dmosConfig = dmosConfig;
         this.httpUtil = httpUtil;
+        this.restTemplate = restTemplate;
     }
 
     public int verifyChannel(String verifyToken){
@@ -29,10 +33,10 @@ public class SocketServerHttpUtil {
         HttpHeaders headers = new HttpHeaders();
         headers.add("token", localToken);
 
-        HashMap<String, String> body = new HashMap<>();
-        body.put("token", verifyToken);
+        DMOSRequest request = new DMOSRequest();
+        request.put("token", verifyToken);
 
-        DMOSResponse response = httpUtil.post(url, "/verify", headers, new Gson().toJson(body));
+        DMOSResponse response = httpUtil.post(url, "/api/verify", headers, request, restTemplate);
         if(response.getCode() != 0)
             return -1;
         return Integer.parseInt(response.getData().get("id"));
@@ -45,8 +49,10 @@ public class SocketServerHttpUtil {
         HttpHeaders headers = new HttpHeaders();
         headers.add("token", localToken);
 
-        Gson gson = new Gson();
-        DMOSResponse response = httpUtil.post(url, "/report", headers, gson.toJson(reportDTO));
+        DMOSRequest request = new DMOSRequest();
+        request.put("report", reportDTO);
+
+        DMOSResponse response = httpUtil.post(url, "/api/report", headers, request, restTemplate);
         return response.getCode();
     }
 }
