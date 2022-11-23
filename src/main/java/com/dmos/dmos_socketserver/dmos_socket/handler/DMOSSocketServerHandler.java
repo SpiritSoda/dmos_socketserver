8f5539676be9ee00f56e2e04a4a41a1a37bd472b
SpiritSoda <1800013119@pku.edu.cn>
2022-11-23 16:48:07 +0800

    [Adjust] compatible with the dmos_server change

--- src/main/java/com/dmos/dmos_socketserver/dmos_socket/handler/DMOSSocketServerHandler.java
+++ src/main/java/com/dmos/dmos_socketserver/dmos_socket/handler/DMOSSocketServerHandler.java
@@ -54,6 +54,7 @@ public class DMOSSocketServerHandler extends ChannelInboundHandlerAdapter {
         }
         else if(message.getType() == MessageType.SERVER_REPORT){
             ServerReportDTO reportDTO = gson.fromJson(message.getData(), ServerReportDTO.class);
+            serverContext.report(reportDTO);
             clientContext.send(reportDTO);
         }
     }
